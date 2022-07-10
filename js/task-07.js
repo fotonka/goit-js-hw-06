b5af3e296251420a5b52d498858128789ba53298
Kirill Chaialo <fotonka098@gmail.com>
2022-07-10 15:41:47 +0300

    9th-ready

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,8 @@
-const textEl = document.querySelector("#text");
-const inputEl = document.querySelector("#font-size-control");
+const textEl = document.querySelector("#text"); // отримали посилання на дом ноду на якийсь елемент з айдішніком text
+const inputEl = document.querySelector("#font-size-control"); // те саме тільки з інпутом
 
 inputEl.addEventListener("input", () => {
-  const size = inputEl.value;
-
-  textEl.style.fontSize = size + "px";
+  // поставили слухач на inputEl
+  const size = inputEl.value; //створили змінну size значення якої inputEl.value(це рядок)
+  textEl.style.fontSize = size + "px"; // додали інлайн стиль для textEl, міняємо розмір шрифта
 });
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanEl = document.querySelector(".color");
+const btnEl = document.querySelector(".change-color");
+btnEl.addEventListener("click", onClick);
+
+let randomColor = getRandomHexColor();
+
+function onClick(event) {
+  const color = (event.currentTarget.style.backgroundColor = randomColor);
+  console.log(color);
+  spanEl.innerText = color;
+  console.log(spanEl);
+}
